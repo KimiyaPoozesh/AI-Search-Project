7cc98c5499e9202d30cc396b0f3ad9e6b59ede99
Kimiya Poozesh <86666504+KimiyaPoozesh@users.noreply.github.com>
2024-09-24 16:01:29 +0330

    Added python

---
+++ Python Part/main.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+from Board import Board
+from Problem import Problem
+from Solution import Solution
+from State import State
+from Search import Search
+import ast
+
+if __name__ == '__main__':
+    test_path = './tests/test2.txt'
+    start_time = datetime.now()
+    file = open(test_path, 'r')
+    p = ''
+    for i in file.readlines():
+        a = i.replace('\n', '')
+        a = a.replace(' ', '')
+        p += a
+    lst = ast.literal_eval(p)
+    s = Search.bfs(Problem(State(Board(len(lst), len(lst[0]), lst), None, 0, None, None)))
+    if s is None:
+        s = Solution(None, None, start_time)
+    else:
+        s.print_path()
+    s.save_to_json_file('test1', 'bfs', 'test2.txt')
